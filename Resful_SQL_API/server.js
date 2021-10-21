fc200abfb8c83b90f91896c2adc6bbb9efb9b0b9
ntpoom9 <zerpoom01@gmail.com>
2021-10-21 21:40:22 +0700

    merge user round1

--- Resful_SQL_API/server.js
+++ Resful_SQL_API/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const restaurantRouter = require('./routes/restaurant.router');
+const customerRouter = require('./routes/customer.router');
 const cors = require('cors')
 
 
@@ -9,21 +10,22 @@ const app = express();
 //Use Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 //Router
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('<h1>This is Restaurant API</h1>'); //โชว์ข้อมูลที่กำหนด
 
 });
 
 // เรียกใช้ router
-app.use("/apis",restaurantRouter)
+app.use("/apis", restaurantRouter)
+app.use("/apis", customerRouter)
 
 
 
 //Run server
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log('Server listening to port 5000')
 })
