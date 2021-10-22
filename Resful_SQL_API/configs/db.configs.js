b2cc83e7de805be055890a6e9c2126610ff6dda4
ntpoom9 <zerpoom01@gmail.com>
2021-10-23 02:56:02 +0700

    PRoject_API for Hot

--- Resful_SQL_API/configs/db.configs.js
+++ Resful_SQL_API/configs/db.configs.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "restaurants",
+    USER: "id17814373_000webhost_zerpoom01",
+    PASSWORD: "<E3Mh4X1nAMw78s)",
+    DB: "id17814373_restaurants",
 };
